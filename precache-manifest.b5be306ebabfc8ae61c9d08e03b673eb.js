1437f2f73ecdd2d08d771e5e0f6979d85e4e95af
Sourav Kumar <sourav.kumar@outlook.com>
2020-07-23 22:18:49 +0530

    Updates

---
+++ precache-manifest.b5be306ebabfc8ae61c9d08e03b673eb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9182f74b6f7f78275a0ae6d75d419362",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "63ebd4e8e9d727f0d963",
+    "url": "/portfolio/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "bc615d9ca3bc22489d7b",
+    "url": "/portfolio/static/css/main.786d56fd.chunk.css"
+  },
+  {
+    "revision": "63ebd4e8e9d727f0d963",
+    "url": "/portfolio/static/js/2.26da98e8.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/portfolio/static/js/2.26da98e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc615d9ca3bc22489d7b",
+    "url": "/portfolio/static/js/main.4730f1d2.chunk.js"
+  },
+  {
+    "revision": "9c2fee6e2ddfed98136b",
+    "url": "/portfolio/static/js/runtime-main.a61f3710.js"
+  },
+  {
+    "revision": "bf07b30ed630a54b61226c1c50f090d8",
+    "url": "/portfolio/static/media/aws.bf07b30e.svg"
+  },
+  {
+    "revision": "fbacd7feb7fb0830790fb37307320a07",
+    "url": "/portfolio/static/media/css.fbacd7fe.svg"
+  },
+  {
+    "revision": "726e6b7ac0d4abfaa868d8a08b100c0a",
+    "url": "/portfolio/static/media/html5.726e6b7a.svg"
+  },
+  {
+    "revision": "64ea780a8682136a557197fabca3b918",
+    "url": "/portfolio/static/media/img1.64ea780a.JPG"
+  },
+  {
+    "revision": "c31a617a3af82b236bb1ca5457168b18",
+    "url": "/portfolio/static/media/img2.c31a617a.JPG"
+  },
+  {
+    "revision": "e6d87e2dca08c953fe3acd1275ed4f4e",
+    "url": "/portfolio/static/media/java.e6d87e2d.svg"
+  },
+  {
+    "revision": "19b511a77052c3ffd7e5f067bee89546",
+    "url": "/portfolio/static/media/javascript.19b511a7.svg"
+  },
+  {
+    "revision": "c45c262e691cf18a9fe9f07f1cee6aab",
+    "url": "/portfolio/static/media/mongodb.c45c262e.svg"
+  },
+  {
+    "revision": "cf955e50a9714a0c7a491d31e808f40c",
+    "url": "/portfolio/static/media/mysql.cf955e50.svg"
+  },
+  {
+    "revision": "11f1518fd8681b2bac799cb25702ab0b",
+    "url": "/portfolio/static/media/nodejs.11f1518f.svg"
+  },
+  {
+    "revision": "bc0165d5331219c2cbdb8c3a479cf635",
+    "url": "/portfolio/static/media/p1.bc0165d5.jpg"
+  },
+  {
+    "revision": "5f99ccaa17966df0a2d0116db135b3f4",
+    "url": "/portfolio/static/media/react.5f99ccaa.svg"
+  }
+]);
